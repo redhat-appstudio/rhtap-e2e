412718d354f0af82b7c58d1d0aba6977f65dcfa8
Flavius Lacatusu <flacatus@redhat.com>
2024-05-27 11:16:10 +0200

    Add integration tests pipelines to execute e2e tests in Konflux CI (#56)
    * Add integration tests pipelines to execute e2e tests in Konflux CI
    
    * Add integration tests pipelines to execute e2e tests in Konflux CI
    
    * Add oras container
    
    * Fix test metadata logs
    
    * Add random characters
    
    * Add random characters
    
    * Fix generator naming

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,8 +1,14 @@
-import { uniqueNamesGenerator, animals } from 'unique-names-generator';
+export function generateRandomChars(length: number): string {
+    if (length < 2) {
+        throw new Error("Length must be at least 2 to satisfy the regex pattern.");
+    }
 
-export function generateRandomName(): string {
-    return uniqueNamesGenerator({
-        dictionaries: [animals,],
-        separator: '-'
-    })
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789-';
+    
+    const firstChar = letters[Math.floor(Math.random() * letters.length)];
+    const middleChars = Array.from({ length: length - 2 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+    const lastChar = 'abcdefghijklmnopqrstuvwxyz0123456789'[Math.floor(Math.random() * 36)]; // 'abcdefghijklmnopqrstuvwxyz0123456789' has 36 characters
+
+    return firstChar + middleChars + lastChar;
 }
