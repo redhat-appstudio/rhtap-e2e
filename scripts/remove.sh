a976816c20f3907fa105a120869803ae7c94f85c
flacatus <flacatus@redhat.com>
2024-06-04 20:46:30 +0200

    add yamllint

--- scripts/remove.sh
+++
@@ -1,7 +0,0 @@
-# Get a list of resource names in the namespace
-resource_names=$(kubectl get applications -n rhtap -o jsonpath='{.items[*].metadata.name}')
-
-# Loop through each resource and remove finalizers
-for resource_name in $resource_names; do
-    kubectl patch application $resource_name -n rhtap --type merge -p '{"metadata":{"finalizers":null}}'
-done
