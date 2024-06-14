310d4f163be8770a94730621de64d943a13ad7cb
Josef Kopriva <jkopriva@redhat.com>
2024-06-14 13:02:55 +0200

    RHTAP-2547 Add delete/import template test
    Signed-off-by: Josef Kopriva <jkopriva@redhat.com>

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,7 +1,7 @@
 import { describe, it } from '@jest/globals';
 
 export const skipSuite = (suiteName: string) => {
-    describe(`skiping suite ${suiteName}`, ()=>{
-        it.skip('', ()=>{})
+    describe(`skiping suite ${suiteName}`, () => {
+        it.skip('', () => { })
     })
 }
