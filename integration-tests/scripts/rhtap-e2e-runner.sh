108929e3ed85131a28df96431befb82a1703a2b4
Flavius Lacatusu <flacatus@redhat.com>
2024-09-05 00:05:36 +0200

    fix: fix bash unbound variables (#145)

--- integration-tests/scripts/rhtap-e2e-runner.sh
+++ integration-tests/scripts/rhtap-e2e-runner.sh
@@ -6,6 +6,21 @@ log() {
     echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] [$1] $2"
 }
 
+#JOB_SPEC env:
+# {
+#     "container_image": "quay.io/rhtap/rhtap-cli@sha256:d2c1a65eda860ff667b30bffde2ec325c1ea7375ae2a68d4defd7aedbd0effdf",
+#     "konflux_component": "rhtap-cli",
+#     "git": {
+#         "pull_request_number": 195,
+#         "pull_request_author": "flacatus",
+#         "git_org": "redhat-appstudio",
+#         "git_repo": "rhtap-cli",
+#         "commit_sha": "ff9aacf8c902f1d6a5004e258522771d48f2b629",
+#         "event_type": "pull_request",
+#         "source_repo_url": "https://github.com/flacatus/rhtap-cli",
+#         "source_repo_branch": "fix_pr"
+#     }
+# }
 load_envs() {
     local rhtap_secrets_file="/usr/local/rhtap-cli-install"
     local konflux_infra_secrets_file="/usr/local/konflux-test-infra"
@@ -15,9 +30,9 @@ load_envs() {
         [ARTIFACT_DIR]="$(mktemp -d)"
         [GITHUB_ORGANIZATION]="rhtap-rhdh-qe"
         [GITLAB_ORGANIZATION]="rhtap-qe"
-        [GIT_REPO]="${GIT_REPO:-""}"
-        [GIT_REVISION]="${GIT_REVISION:-"main"}"
-        [GIT_URL]="${GIT_URL:-""}"
+        [GIT_REPO]="$(echo "$JOB_SPEC" | jq -r '.git.git_repo')"
+        [GIT_REVISION]="$(echo "$JOB_SPEC" | jq -r '.git.commit_sha')"
+        [GIT_URL]="$(echo "$JOB_SPEC" | jq -r '.git.source_repo_url')"
         [IMAGE_REGISTRY]="$(kubectl -n rhtap-quay get route rhtap-quay-quay -o  'jsonpath={.spec.host}')"
         [NODE_TLS_REJECT_UNAUTHORIZED]=0
         [OCI_CONTAINER]="${OCI_CONTAINER:-""}"
