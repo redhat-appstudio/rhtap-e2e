c885ef0258b3ba1c96f0d7a17c61c9b8daf99055
chetna14manku <rchetna101@gmail.com>
2025-02-18 17:01:54 +0530

    rebase update

--- tests/gpts/bitbucket/configs/bitbucket_positive_suite.ts
+++ tests/gpts/bitbucket/configs/bitbucket_positive_suite.ts
@@ -177,7 +177,7 @@ export const bitbucketSoftwareTemplateTests = (gptTemplate: string, stringOnRout
          * if failed to figure out the image path ,return pod yaml for reference
          */
         it(`Check ${gptTemplate} pipelinerun yaml has the rh-syft image path`, async () => {
-            const result = await verifySyftImagePath(kubeClient, repositoryName, ciNamespace);
+            const result = await verifySyftImagePath(kubeClient, repositoryName, ciNamespace, 'push');
             expect(result).toBe(true);
         }, 900000);
 
@@ -185,7 +185,7 @@ export const bitbucketSoftwareTemplateTests = (gptTemplate: string, stringOnRout
          * verify if the ACS Scan is successfully done from the logs of task steps
          */
         it(`Check if ACS Scan is successful for ${gptTemplate}`, async () => {
-            const result = await checkIfAcsScanIsPass(kubeClient, repositoryName, ciNamespace);
+            const result = await checkIfAcsScanIsPass(kubeClient, repositoryName, ciNamespace, 'push');
             expect(result).toBe(true);
             console.log("Verified as ACS Scan is Successful");
         }, 900000);
